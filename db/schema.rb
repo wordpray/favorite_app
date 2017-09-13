9c270983dce586b61e5cc998939e1ff39e1e52bb
y.yoshiki <labiatejoker@gmail.com>
2017-09-13 20:08:30 +0900

    Add name column in Usertable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913101915) do
+ActiveRecord::Schema.define(version: 20170913105602) do
 
   create_table "links", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "title",      limit: 65535, null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20170913101915) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "nickname"
+    t.string   "name",                                null: false
   end
 
   add_foreign_key "links", "lists"
