87b67ae901afe17b5313041e2f37ada232f3f6c0
y.yoshiki <labiatejoker@gmail.com>
2017-09-15 13:41:28 +0900

    Add description column in Lists table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170913105602) do
+ActiveRecord::Schema.define(version: 20170915043921) do
 
   create_table "links", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "title",      limit: 65535, null: false
@@ -24,10 +24,11 @@ ActiveRecord::Schema.define(version: 20170913105602) do
   end
 
   create_table "lists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "title",      limit: 65535, null: false
+    t.text     "title",       limit: 65535, null: false
+    t.text     "description", limit: 65535
     t.integer  "user_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
     t.index ["user_id"], name: "index_lists_on_user_id", using: :btree
   end
 
