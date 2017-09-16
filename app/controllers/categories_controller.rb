09b290803f99af700465c2690676ade6a032098a
y.yoshiki <labiatejoker@gmail.com>
2017-09-17 02:29:37 +0900

    Create categories controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,26 @@
+class CategoriesController < ApplicationController
+  def new
+    @user = current_user
+    @category = @user.categories.new
+  end
+
+  def create
+    @user = current_user
+    @category = @user.categories.new(category_params)
+    if @category.save
+      redirect_to root_url
+    else
+      render action: "new"
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:title)
+  end
+
+end
