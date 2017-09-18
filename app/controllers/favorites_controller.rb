636e0c7ab6ad7fcd5e992940c4cf66952f33bbc4
y.yoshiki <labiatejoker@gmail.com>
2017-09-18 15:22:35 +0900

    Modify eroor

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,10 @@
 class FavoritesController < ApplicationController
   def home
-    @user = current_user
-    @categories = @user.categories.order('title ASC')
+    @user           = current_user
+    @categories     = @user.categories.order('title ASC')
     @first_category = @categories.first
-    @lists = @first_category.lists
-    @first_list = @lists.first
+    @lists          = @first_category.lists
+    @first_list     = @lists.first
   end
 
   def about
