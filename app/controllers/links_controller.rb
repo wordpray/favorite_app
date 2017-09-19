bf0eb1adbe92deaa637c1222e18debe79a8a7cb3
y.yoshiki <labiatejoker@gmail.com>
2017-09-19 16:50:44 +0900

    Create "Continue to create"  button in link form.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -11,11 +11,45 @@ class LinksController < ApplicationController
   def create
     @user = current_user
     @link = @user.links.new(link_params)
-    if @link.save
-      redirect_to root_url
+
+    #Createボタンをクリック
+    if params[:commit]
+      if @link.save
+        redirect_to root_url
+      else
+        flash[:danger] = "Failed to create"
+        @link = Link.new
+        @list = List.find(params[:list_id])
+        @links = @list.links.new
+        @lists = @user.lists
+        render action: "new"
+      end
+    #Continue to createボタンをクリック
+    elsif params[:next_button]
+      if @link.save
+        @link = Link.new
+        @list = List.find(params[:list_id])
+        @links = @list.links.new
+        @lists = @user.lists
+        flash[:success] = "Successful creation"
+        render action: "new"
+      else
+        flash[:danger] = "Failed to create"
+        @link = Link.new
+        @list = List.find(params[:list_id])
+        @links = @list.links.new
+        @lists = @user.lists
+        render action: "new"
+      end
     else
+      flash[:danger] = "Failed to create"
+      @link = Link.new
+      @list = List.find(params[:list_id])
+      @links = @list.links.new
+      @lists = @user.lists
       render action: "new"
     end
+
   end
 
   def edit
