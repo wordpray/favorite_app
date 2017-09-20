f3f2609600f6f4233165f2ebaa9a4faf0f0830a4
y.yoshiki <labiatejoker@gmail.com>
2017-09-20 16:35:34 +0900

    Create genre database

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  belongs_to :genre
   has_many :links, dependent: :destroy
   validates :title, presence: true, uniqueness: { scope: [:category_id] }
 end
